e15d95ea2e46d6bed03caf692385134780728224
James Ranous <ranousjf@hotmail.com>
2017-06-21 23:47:17 -0400

    Implemented Service Repository

--- PeopleViewer/MainWindow.xaml.cs
+++ PeopleViewer/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using PersonRepository.Interface;
 using System.Windows;
+using PersonRepository.Service;
 
 namespace PeopleViewer
 {
@@ -12,6 +13,14 @@ namespace PeopleViewer
 
         private void ServiceFetchButton_Click(object sender, RoutedEventArgs e)
         {
+            ClearListBox();
+
+            IPersonRepository repository = new ServiceRepository();
+            var people = repository.GetPeople();
+            foreach (var person in people)
+                PersonListBox.Items.Add(person);
+
+            ShowRepositoryType(repository);
 
         }
 
